ff143429ef3337c7312af57adfdeb75ea83e12a5
katalla1 <katalla@hotmal.com>
2021-09-29 15:24:49 -0700

    added orientation fixed buttons

--- app/assets/screens/LoginScreen.js
+++ app/assets/screens/LoginScreen.js
@@ -1,34 +1,41 @@
 import React from 'react'
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View, Button } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import AppButton from '../../components/AppButton';
 import colors from '../config/colors';
 export default function LoginScreen() {
     return (
-        <ImageBackground 
-        style= {styles.background}
-        source= {require("../../assets/backround.jpg")}
+        <ImageBackground style={{ flex:1, justifyContent: "center"}}       
+            source= {require("../../assets/background.jpg")}
         >
-         <View style = {styles.buttonsContainer}>    
-        <AppButton title= "Login" />
-        <AppButton title= "Register" />    
-        </View>
-
-
+            <View style= {styles.loginButton}>
+                <AppButton  title= "Login" />
+            </View>
+            <View style= {styles.registerButton}>
+                <AppButton  title= "Register"/>
+            </View>
+            
         </ImageBackground>
+        
     )
 }
 
 const styles = StyleSheet.create({
 
-background: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",  
+loginButton: {
+    position: "absolute",
+    bottom: 100,
+    left: 20,
+    width: "40%",
+    height: 30,
     
 },
-buttonsContainer: {
-    padding: 20,
-    width: "80%",
-}
+registerButton: {
+    position: "absolute",
+    bottom: 100,
+    right: 20,
+    width: "40%",
+    height: 30,
+},
 
 })
